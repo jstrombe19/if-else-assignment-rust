7e40720ef338e2c90613b7e32a236f9072b8151f
Jared Stromberg <jaredstromberg@gmail.com>
2021-01-18 15:42:21 -0700

    Modified brute solution to defensively account for capitalization of the letter entered by the user.

--- if-else.rs
+++ if-else.rs
@@ -12,12 +12,13 @@ else F
 use std::io;
 
 fn main() {// 1
-let mut a = String::new();
-println!("Enter a letter");
-io::stdin().read_line(&mut a).expect("Failed");
-a = a.trim().to_string().parse().expect("Failed");
-if a == "a" || a == "e" || a == "i" || a == "o" || a == "u" || a == "y" {
-  println!("{} is a vowel!", a);
-} else {
-  println!("{} is not a vowel.", a);
-}}
+  let mut a = String::new();
+  println!("Enter a letter");
+  io::stdin().read_line(&mut a).expect("Failed");
+  a = a.trim().to_string().parse().expect("Failed");
+  if a.to_lowercase() == "a" || a.to_lowercase() == "e" || a.to_lowercase() == "i" || a.to_lowercase() == "o" || a.to_lowercase() == "u" || a.to_lowercase() == "y" {
+    println!("{} is a vowel!", a);
+  } else {
+    println!("{} is not a vowel.", a);
+  }
+}
